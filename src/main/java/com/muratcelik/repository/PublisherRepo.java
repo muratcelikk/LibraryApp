e850f4f555d89f77975b261c39601bbdd8a50978
Murat Çelik <muratcelik11@gmail.com>
2020-11-24 06:34:26 +0300

    ..

--- src/main/java/com/muratcelik/repository/PublisherRepo.java
+++
@@ -1,7 +0,0 @@
-package com.muratcelik.repository;
-
-import com.muratcelik.entity.Publisher;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PublisherRepo extends JpaRepository<Publisher, Long> {
-}
